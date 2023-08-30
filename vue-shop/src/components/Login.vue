8325888ac228c7292d42384b43f4cbb736408d7e
ccy <1909786769@qq.com>
2023-08-30 23:29:00 +0800

    完成了登陆界面

---
+++ vue-shop/src/components/Login.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="login_container">
+        <div class="login_box">
+            <div class="head_box">
+                <img src="../assets/1.jpg" alt="">
+            </div>
+     <el-form  ref="loginref"  :rules="rules" :model="loginform" class="login_input">
+  <el-form-item prop="username">
+    <el-input v-model="loginform.username" prefix-icon="el-icon-user-solid"
+></el-input>
+  </el-form-item>
+  <el-form-item prop="password">
+    <el-input type="password" v-model="loginform.password" prefix-icon="el-icon-view"
+></el-input>
+  </el-form-item>
+  <div class="login_button">
+  <el-button @click="loginrefvite">登录</el-button>
+<el-button @click="resetloginref">重置</el-button></div>
+</el-form>
+
+        </div>
+    </div>
+</template>
+<script>
+    export default{
+       data(){
+        return{
+            loginform:{
+                username:'',
+                password:''
+            },
+             rules: {
+          username:[
+            { required: true, message: '请输入用户名', trigger: 'blur' },
+            { min: 2, max: 6, message: '长度在 2 到 6 个字符', trigger: 'blur' }
+          ],
+          password:[
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            { min: 6, max: 10, message: '长度在 6 到 10 个字符', trigger: 'blur' }
+          ]
+            }
+        }
+       },
+       methods:{
+        resetloginref(){
+            this.$refs.loginref.resetFields();
+        },
+        loginrefvite(){
+            this.$refs.loginref.validate((valid)=>{
+              if(valid==true){
+                this.$router.push('/home')
+              }
+            });
+        }
+       }
+    }
+</script>
+<style lang="less" scoped>
+.login_container{
+    background-color: #2e4e6e;
+    background-size: 100% 100%;
+    height: 100% ;
+    width: 100%;
+    }
+.login_box{
+        width: 450px;
+        height: 300px;
+        background-color: #fff;
+        border-radius: 4px;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%,-50%);
+        
+    }
+    .head_box{
+        width: 100px;
+        height: 100px;
+        border: 1px solid #eee;
+        border-radius: 50%;
+        padding: 10px;
+        box-shadow: 0 0 10px #ddd;
+        position: absolute;
+        left: 50%;
+        background-color: #fff;
+        
+        transform: translate(-50%,-50%);
+
+        img{
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background-color: #eee;
+        }
+    }
+    .login_input{
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        padding: 50px 60px;
+        box-sizing: border-box;
+    }
+    .login_button{
+        display: flex;
+        justify-content: center;
+        padding: 10px;
+        button{
+            margin:  0 30px;
+        }
+    }
+</style>
--- vue-shop/src/main.js
+++ vue-shop/src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
+import router from './router'
+import'./assets/css/global.css'
+import axios from 'axios'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
